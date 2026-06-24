9848ead55b2ec1db532276afe86c2682a312a5e7
Ayieee98 <tajuddin6565@gmail.com>
2026-06-24 12:42:56 +0800

    update

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://xlsosjhrqyjroipowwdq.supabase.co/rest/v1/'
+const supabaseUrl = 'https://xlsosjhrqyjroipowwdq.supabase.co'
 const supabaseKey = 'sb_publishable_ewTZ0PemwqQBRW_U8HK7LQ_ftuKZafB'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
